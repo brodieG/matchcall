020c134cb6f3cc2e05c4d6cab3f8975999d0032c
brodieG <brodieG@users.noreply.github.com>
2014-10-23 22:04:28 -0400

    fixed test for change from par.offset to n

--- tests/unitizer/matchcall.R
+++ tests/unitizer/matchcall.R
@@ -4,7 +4,7 @@ unitizer_sect("Input Errors", {
   match_call(dots=1)
   match_call(dots="explode")
   match_call(dots=NA_character_)
-  match_call(parent.offset=0)
+  match_call(0)
   match_call(default.formals=c(TRUE, FALSE))
   match_call(empty.formals=NA)
 })

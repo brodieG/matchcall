e151668f090f4dcd0c56834516e77f5a5078d8b5
brodieG <brodieG@users.noreply.github.com>
2014-10-25 21:55:58 -0400

    Doc updates

---
+++ inst/doc/matchcallissues.R
@@ -0,0 +1,33 @@
+## ------------------------------------------------------------------------
+fun0 <- function(...) {
+  fun_gpar <- function(b, ...) {
+    fun_par <- function(a)
+      match.call(fun_gpar, sys.call(sys.parent()), expand.dots=FALSE)
+    fun_par()
+  }
+  fun_gpar(...)
+}
+fun0(999, 2 + 2, 3 * pi(), 4)
+
+## ------------------------------------------------------------------------
+fun2 <- function(a, ...) {
+  fun_gpar <- function(b, ...) {
+    (function(...) match.call(fun_gpar, sys.call(sys.parent()), expand.dots=F))()
+  }
+  fun_gpar(a, ...)
+}
+
+fun2(3, "test", x=45, zest="lemon")
+
+## ------------------------------------------------------------------------
+fun3 <- function(a, ...) {
+  fun_gpar <- function(b, c, d, ...) {
+    (function() match.call(fun_gpar, sys.call(sys.parent()), expand.dots=F))()
+  }
+  fun_gpar(a, ...)
+}
+fun3(3, "test", 59, x=45, zest="lemon", 58)
+
+## ------------------------------------------------------------------------
+fun3(3, "test", 59, x=45, zest="lemon", (58), (60))
+

84d13fbdf40635f6a1ffa84ec4d6bba1147723fa
brodieG <brodieG@users.noreply.github.com>
2014-10-31 19:11:01 -0400

    updated valgrind tests with all normal tests

--- tests/valgrind/matchcall.valgrind.R
+++ tests/valgrind/matchcall.valgrind.R
@@ -2,12 +2,12 @@ library(matchcall)
 
   gctorture(TRUE)
 
-  # try(match_call(dots=1))
-  # try(match_call(dots="explode"))
-  # try(match_call(dots=NA_character_))
-  # try(match_call(0))
-  # try(match_call(default.formals=c(TRUE, FALSE)))
-  # try(match_call(empty.formals=NA))
+  try(match_call(dots=1))
+  try(match_call(dots="explode"))
+  try(match_call(dots=NA_character_))
+  try(match_call(0))
+  try(match_call(default.formals=c(TRUE, FALSE)))
+  try(match_call(empty.formals=NA))
 
   fun <- function(x, y, z=TRUE) match_call()
 
@@ -32,6 +32,7 @@ library(matchcall)
   fun4(1, 2, 3, 4)
   fun4(1, 2, x=3, w=4)
   fun4(x=3, 1, 2, list(4))
+
   fun0 <- function(...) {
     fun_gpar <- function(b, ...) {
       fun_par <- function(a)
@@ -73,6 +74,19 @@ library(matchcall)
   fun2 <- function(c, d) fun1(a + 1, b - 1)
   fun2(25, pi() + 3)
 
+  fun <- function(a, b, c=TRUE, ...)
+    match_call(default.formals=TRUE, dots="include")
+  fun(5, 6, x=list(1:10, FALSE))
+
+  fun3a <- function(x) fun4a()
+  fun4a <- function() match_call(2)
+
+  fun3b <- function(x) fun4b()  # Note: fun4b defined outside fun3b
+  fun4b <- function() match.call(definition=fun3b, call=sys.call(sys.parent()))
+
+  fun3a(1 + 1)       # `match_call` works
+  fun3b(1 + 1)       # `match.call` also works, but only if we explicitly specify `definition`
+
   fun1 <- function(x, y, z=TRUE, w=letters[1:3]) match_call(default.formals=TRUE)
 
   fun1(1, 2, 3)
@@ -109,6 +123,37 @@ library(matchcall)
   fun4 <- function(x, y, ..., z, w=letters[1:3])
     match_call(empty.formals=TRUE, dots="expand")
 
-  fun4()   # this is crashing R right now...
+  fun4()   # Expand dots even if empty, should disappear
+
+  fun1 <- function(x, y, z=TRUE, w=letters[1:3])
+    match_call(user.formals=FALSE, default.formals=TRUE)
+
+  fun1()
+  fun1(z=3)
+  fun1(z=3, w=5)
+  fun1(1, 2)
+
+  fun2 <- function(x, y, z=TRUE, w=letters[1:3])
+    match_call(user.formals=FALSE, default.formals=TRUE, empty.formals=TRUE)
+
+  fun2()
+  fun2(z=3)
+  fun2(z=3, w=5)
+  fun2(1, 2)
+
+  fun3 <- function(x, y, ..., z=TRUE, w=letters[1:3])
+    match_call(user.formals=FALSE, default.formals=TRUE, empty.formals=TRUE)
+
+  fun3()
+  fun3(z=3)
+  fun3(z=3, w=5)
+  fun3(z=3, w=5, q="hello")
+  fun3(1, 2)
+
+  fun4 <- function(x, y, ..., z=TRUE, w=letters[1:3])
+    match_call(user.formals=FALSE, default.formals=TRUE, empty.formals=TRUE, dots="include")
+
+  fun4()
+  fun3(z=3, w=5, q="hello")  # should drop dots
 
   gctorture(FALSE)
